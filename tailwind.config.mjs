f2ec2bc70dea48a4f7c5b3c8b38a869025994c77
Daniel Arauz <daniel.arauz@gmail.com>
2023-11-28 01:58:17 +0100

    finished?

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,6 +5,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			borderRadius:{
+				'5xl': '3rem',
+			},
 			keyframes: {
 				materialize: {
 					'0%': {
