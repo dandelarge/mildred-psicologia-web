f84ca2406309618b82038a2b6adc01356286abc4
Daniel Arauz <=>
2024-01-28 11:21:01 +0100

    added sitemap and compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import alpinejs from "@astrojs/alpinejs";
+import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs(), sitemap(), compress()]
 });
